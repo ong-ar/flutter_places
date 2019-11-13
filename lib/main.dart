19416a1d491b6050cc96ff239134b25759e9341d
ong-ar <reclusis@gmail.com>
2019-11-13 22:28:36 +0900

    store picture

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/add_place_screen.dart';
 import './screens/places_list_screen.dart';
+
+import './providers/great_places.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.amber,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: GreatPlaces(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlacesListScreen(),
+        routes: {
+          AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+        },
       ),
-      home: PlacesListScreen(),
-      routes: {
-        AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
-      },
     );
   }
 }
--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,1 +1,16 @@
-class Place {}
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+
+class Place {
+  final String id;
+  final File image;
+  final String title;
+  final String location;
+
+  Place({
+    @required this.id,
+    @required this.image,
+    @required this.title,
+    this.location,
+  });
+}
