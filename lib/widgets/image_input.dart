3ef52474405f0d24c1bdfae7e78621f1b885894f
ong-ar <reclusis@gmail.com>
2019-11-13 22:30:02 +0900

    error handling

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -22,6 +22,11 @@ class _ImageInputState extends State<ImageInput> {
       source: ImageSource.camera,
       maxWidth: 600,
     );
+
+    if (imageFile == null) {
+      return;
+    }
+
     setState(() {
       _storedImage = imageFile;
     });
